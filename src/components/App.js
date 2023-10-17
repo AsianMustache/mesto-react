0b9900f525a71bc038b0a881d2ceef35dfcc789d
AsianMustache <phany280687@mail.ru>
2023-10-17 17:44:08 +0300

    feat: add cards and edit profile

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
@@ -29,6 +29,27 @@ function App() {
         setIsEditAvatarPopupOpen(false);
     }
 
+    useEffect(() => {
+        function handleEscClose(event) {
+            if (event.key === 'Escape') {
+                closeAllPopups();
+            }
+        }
+
+        function handleOverlayClose(event) {
+            if (event.target.classList.contains('popup')) {
+                closeAllPopups();
+            }
+        }
+
+        window.addEventListener('keydown', handleEscClose);
+        window.addEventListener('mousedown', handleOverlayClose);
+
+        return () => {
+            window.removeEventListener('keydown', handleEscClose);
+            window.removeEventListener('mousedown', handleOverlayClose);
+        };
+    }, []);
 
   return (
     <div className="page">
@@ -73,67 +94,6 @@ function App() {
         </PopupWithForm>
 
         <ImagePopup />
-        {/* <div className="popup popup_form_edit">
-            <div className="popup-container">
-                <button type="button" className="popup-container__close-button popup-close"></button>
-                <h2 className="popup-container__title">Редактировать профиль</h2>
-                <form name="edit-form" className="edit-form popup__form" noValidate>
-                    <label className="popup__label">
-                        <input type="text" id="name" name="name" value="Жак-Ив Кусто" placeholder="Введите имя" className="edit-form__text edit-form__text_input_name popup__input" minLength="2" maxLength="40" required />
-                        <span className="name-error popup__input-error"></span>
-                    </label>
-                    <label className="popup__label">
-                        <input type="text" id="description"  name="description" placeholder="Введите профессию" value="Исследователь океана" className="edit-form__text edit-form__text_input_description popup__input" minLength="2" maxLength="200" required />
-                        <span className="description-error popup__input-error"></span>
-                    </label>
-                    <button type="submit" className="edit-form__container-button popup__button">Сохранить</button>
-                </form>
-            </div>
-        </div>
-        <div className="popup popup_form_add">
-            <div className="popup-container">
-                <button type="button" className="popup-container__add-popup-close-button popup-close"></button>
-                <h2 className="popup-container__title">Новое место</h2>
-                <form name="add-form" className="add-form popup__form" noValidate>
-                    <label className="popup__label">
-                        <input type="text" id="name-place" name="name-place"  placeholder="Название" className="add-form__text add-form__text_input_title popup__input" minLength="2" maxLength="40" required />
-                        <span className="name-place-error popup__input-error"></span>
-                    </label>
-                    <label className="popup__label">
-                        <input type="url" name="url" id="url" placeholder="Ссылка на картинку" className="add-form__text add-form__text_input_url popup__input" required />
-                        <span className="url-error popup__input-error"></span>
-                    </label>
-                    <button type="submit" className="add-form__container-button popup__button">Создать</button>
-                </form>
-            </div>
-        </div>
-        <div className="popup popup_form_image">
-            <div className="popup-image-container">
-                <img className="popup-image-container__image-fullscreen" alt="" />
-                <h2 className="popup-image-container__title-fullscreen"></h2>
-                <button className="popup-image-container__close-button popup-close" type="button"></button>
-            </div>
-        </div>
-        <div className="popup popup_form_delete">
-            <div className="popup-container">
-                <button type="button" className="popup-container__close-button popup-close"></button>
-                <h2 className="popup-container__title-delete">Вы уверены?</h2>
-                <button type="button" className="popup-container__delete-button popup__button">Да</button>
-            </div>
-        </div>
-        <div className="popup popup_form_edit-avatar">
-            <div className="popup-container">
-                <button type="button" className="popup-container__add-popup-close-button popup-close"></button>
-                <h2 className="popup-container__title-avatar-edit">Обновить аватар</h2>
-                <form name="avatar-edit-form" className="avatar-edit-form popup__form" noValidate>
-                    <label className="popup__label">
-                        <input type="url" name="url" id="url-avatar" placeholder="Ссылка на аватар" className="avatar-edit-form__input-url popup__input" required />
-                        <span className="url-avatar-error popup__input-error"></span>
-                    </label>
-                    <button type="submit" className="avatar-edit-form__button popup__button">Сохранить</button>
-                </form>
-            </div>
-        </div> */}
 
     </div>
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,34 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import edit from '../images/editButton.svg';
 import plus from '../images/plus.svg';
 import trash from '../images/Trash.svg';
 import like from '../images/favorite.svg';
+import api from "../utils/Api";
 
 function Main({ onEditProfile, onAddPlace, onEditAvatar }) {
+    const [userName, setUserName] = useState('');
+    const [userDescription, setUserDescription] = useState('');
+    const [userAvatar, setUserAvatar] = useState('');
+    const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+        api.getApiUserInfo()
+            .then((data) => {
+                setUserName(data.name);
+                setUserDescription(data.about);
+                setUserAvatar(data.avatar);
+            })
+            .catch((err) => {
+                console.log('Ошибка:', err);
+            });
+
+    api.getAllCards()
+        .then((data) => {
+            setCards(data);
+        })
+        .catch((err) => {
+            console.log('Ошибка:', err);
+        });
+    }, []);
 
     return(
         <main className="content">
             <section className="profile">
-                <button id="profile-avatar" className="profile__avatar" onClick={onEditAvatar} ></button>
+                <button id="profile-avatar" className="profile__avatar" onClick={onEditAvatar} style={{ backgroundImage: `url(${userAvatar})`}} ></button>
                 <div className="profile__info">
-                  <h1 id="profile-name" className="profile__info-name"></h1>
-                  <p id="profile-description" className="profile__info-description"></p>
+                  <h1 id="profile-name" className="profile__info-name">{userName}</h1>
+                  <p id="profile-description" className="profile__info-description">{userDescription}</p>
                   <button type="button" className="profile__info-edit-button" onClick={onEditProfile}><img src={edit} className="profile__info-edit-button-image" alt="Редактирование кнопка" /></button>
                 </div>
                 <button type="button" className="profile__add-button" onClick={onAddPlace}><img src={plus} alt="Добавление" className="profile__add-button-image" /></button>
               </section>
             <section className="elements">
-            <template id="template-elements">
-                <article className="element">
+            {cards.map((card, index) => {
+                return(
+                <article className="element" key={index}>
                     <button type="button" className="element__delete-button"><img className="element__image-delete" src={trash} alt="Кнопка удаления" /></button>
-                    <img className="element__image" />
+                    <img className="element__image" style={{ backgroundImage: `url(${card.link})` }} alt={card.name}/>
                     <div className="element__group">
-                        <h2 className="element__group-title"></h2>
+                        <h2 className="element__group-title">{card.name}</h2>
                         <button type="button" className="element__group-button"><img className="element__group-favorite" alt="Избранное" src={like} /></button>
                     </div>
-                    <span id="element__likes" className="element__likes"></span>
-                </article>
-            </template>
+                    <span id="element__likes" className="element__likes">{card.likes.length}</span>
+                </article>)
+            })}
             </section>
         </main>
     )
