64361132f80707f5022a1a3560056922787dba9d
AsianMustache <phany280687@mail.ru>
2023-10-18 12:16:34 +0300

    fix: fix fullscreen image

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,24 +1,12 @@
 import React from "react";
 
-// function ImagePopup({ card, onClose }) {
-//     return (
-//         <div className="popup popup_form_image">
-//             <div className="popup-image-container">
-//                 <img className="popup-image-container__image-fullscreen" alt="" />
-//                 <h2 className="popup-image-container__title-fullscreen"></h2>
-//                 <button className="popup-image-container__close-button popup-close" type="button"></button>
-//             </div>
-//         </div>
-//     );
-// }
-
 function ImagePopup({ card, onClose }) {
     return (
-      <div className={`popup ${card ? 'popup_opened' : ''}`}>
-        <div className="popup__container">
-          <button type="button" className="popup__close-button" onClick={onClose}></button>
-          <img src={card?.link} alt={card?.name} className="popup__image" />
-          <p className="popup__caption">{card?.name}</p>
+      <div className={`popup popup_form_image ${card ? 'popup_opened' : ''}`}>
+        <div className="popup-image-container">
+          <img src={card?.link} alt={card?.name} className="popup-image-container__image-fullscreen" />
+          <h2 className="popup-image-container__title-fullscreen">{card?.name}</h2>
+          <button type="button" className="popup-image-container__close-button popup-close" onClick={onClose}></button>
         </div>
       </div>
     );
