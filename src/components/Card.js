b4a2d39d89204c84c7c4fc6533517d37ee94a3c4
AsianMustache <phany280687@mail.ru>
2023-10-30 19:09:09 +0300

    feat: add context to Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,29 @@
 import React from "react";
 import trash from '../images/Trash.svg';
 import like from '../images/favorite.svg';
+import { useContext } from "react";
 
 function Card({ card, onCardClick }) {
     function handleClick() {
         onCardClick(card);
     }
+
+    const currentUser = useContext(CurrentUserContext);
+    const isOwn = card.owner._id === currentUser._id;
+    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    const cardLikeButtonClassName = ( 
+        `element__group-button ${isLiked ? 'element__group-button_active' : ''}`
+    );; 
+
     return(
         <article className="element">
-            <button type="button" className="element__delete-button"><img className="element__image-delete" src={trash} alt="Кнопка удаления" /></button>
+             {isOwn && <button type="button" className="element__delete-button"><img className="element__image-delete" src={trash} alt="Кнопка удаления" /></button>}
+            {/* <button type="button" className="element__delete-button"><img className="element__image-delete" src={trash} alt="Кнопка удаления" /></button> */}
             <img className="element__image" style={{ backgroundImage: `url(${card.link})` }} src={card.link} alt={card.name} onClick={handleClick} />
             <div className="element__group">
                 <h2 className="element__group-title">{card.name}</h2>
-                <button type="button" className="element__group-button"><img className="element__group-favorite" alt="Избранное" src={like} /></button>
+                {/* <button type="button" className="element__group-button"><img className="element__group-favorite" alt="Избранное" src={like} /></button> */}
+                <button type="button" className={cardLikeButtonClassName}><img className="element__group-favorite" alt="Избранное" src={like} /></button>
             </div>
             <span id="element__likes" className="element__likes">{card.likes.length}</span>
         </article>)
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,23 +3,25 @@ import edit from '../images/editButton.svg';
 import plus from '../images/plus.svg';
 import api from "../utils/Api";
 import Card from "./Card";
+import { useContext } from "react";
 
 function Main({ onEditProfile, onAddPlace, onEditAvatar, onCardClick }) {
-    const [userName, setUserName] = useState('');
-    const [userDescription, setUserDescription] = useState('');
-    const [userAvatar, setUserAvatar] = useState('');
+    // const [userName, setUserName] = useState('');
+    // const [userDescription, setUserDescription] = useState('');
+    // const [userAvatar, setUserAvatar] = useState('');
     const [cards, setCards] = useState([]);
+    const currentUser = useContext(CurrentUserContext);
 
     useEffect(() => {
-        api.getApiUserInfo()
-            .then((data) => {
-                setUserName(data.name);
-                setUserDescription(data.about);
-                setUserAvatar(data.avatar);
-            })
-            .catch((err) => {
-                console.log('Ошибка:', err);
-            });
+        // api.getApiUserInfo()
+        //     .then((data) => {
+        //         setUserName(data.name);
+        //         setUserDescription(data.about);
+        //         setUserAvatar(data.avatar);
+        //     })
+        //     .catch((err) => {
+        //         console.log('Ошибка:', err);
+        //     });
 
     api.getAllCards()
         .then((data) => {
@@ -33,10 +35,10 @@ function Main({ onEditProfile, onAddPlace, onEditAvatar, onCardClick }) {
     return(
         <main className="content">
             <section className="profile">
-                <button id="profile-avatar" className="profile__avatar" onClick={onEditAvatar} style={{ backgroundImage: `url(${userAvatar})`}} ></button>
+                <button id="profile-avatar" className="profile__avatar" onClick={onEditAvatar} style={{ backgroundImage: `url(${currentUser ? currentUser.avatar : ''})`}} ></button>
                 <div className="profile__info">
-                  <h1 id="profile-name" className="profile__info-name">{userName}</h1>
-                  <p id="profile-description" className="profile__info-description">{userDescription}</p>
+                  <h1 id="profile-name" className="profile__info-name">{currentUser ? currentUser.name : ''}</h1>
+                  <p id="profile-description" className="profile__info-description">{currentUser ? currentUser.about : ''}</p>
                   <button type="button" className="profile__info-edit-button" onClick={onEditProfile}><img src={edit} className="profile__info-edit-button-image" alt="Редактирование кнопка" /></button>
                 </div>
                 <button type="button" className="profile__add-button" onClick={onAddPlace}><img src={plus} alt="Добавление" className="profile__add-button-image" /></button>
